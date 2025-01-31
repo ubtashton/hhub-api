d58d65ee24f031b292b286d92348b3ac3d902031
Ashton Smith <ashton.smith@ubteam.com>
2025-01-31 14:54:59 +1300

    error handling

--- app/application/config/database.php
+++ app/application/config/database.php
@@ -72,25 +72,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $active_group = 'default';
 $query_builder = TRUE;
+if (getenv('DB_HOST')) {
 
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => getenv('DB_HOST') !== false ? getenv('DB_HOST') : 'localhost',
-	'username' => getenv('DB_USER') !== false ? getenv('DB_USER') : '',
-	'password' => getenv('DB_PASS') !== false ? getenv('DB_PASS') : '',
-	'database' => getenv('DB_NAME') !== false ? getenv('DB_NAME') : '',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
+	$db['default'] = array(
+		'dsn'	=> '',
+		'hostname' => getenv('DB_HOST') !== false ? getenv('DB_HOST') : 'localhost',
+		'username' => getenv('DB_USER') !== false ? getenv('DB_USER') : '',
+		'password' => getenv('DB_PASS') !== false ? getenv('DB_PASS') : '',
+		'database' => getenv('DB_NAME') !== false ? getenv('DB_NAME') : '',
+		'dbdriver' => 'mysqli',
+		'dbprefix' => '',
+		'pconnect' => FALSE,
+		'db_debug' => (ENVIRONMENT !== 'production'),
+		'cache_on' => FALSE,
+		'cachedir' => '',
+		'char_set' => 'utf8',
+		'dbcollat' => 'utf8_general_ci',
+		'swap_pre' => '',
+		'encrypt' => FALSE,
+		'compress' => FALSE,
+		'stricton' => FALSE,
+		'failover' => array(),
+		'save_queries' => TRUE
+	);
+}
